fd770a6718c4cf4b56ed793ed78981f391d0f86c
david he man <david.he.is.here@gmail.com>
2019-08-18 14:14:28 -0500

    created mapbox folder in routes and places.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
-var users = require('./routes/users');
+var places = require('./routes/mapbox-api/places');
 require('dotenv').config()
 
 var app = express();
@@ -25,7 +25,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/users', users);
+app.use('/places', places);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
