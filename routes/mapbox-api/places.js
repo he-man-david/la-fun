5b06fedfa1e459fa4d9b9450f0e3b51a36b697f7
david he man <david.he.is.here@gmail.com>
2019-08-22 22:53:17 -0500

    got the api to work...

--- routes/mapbox-api/places.js
+++ routes/mapbox-api/places.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var router = express.Router();
+const fetch = require('node-fetch');
 
 router.get('/', function(req, res, next) {
-  res.send("hello from places.js mapbox");
+  fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/coffee.json?access_token=${process.env.MAPBOX_KEY}`
+        + "&types=poi&proximity=-96.95831539999999%2C32.8966877")
+        .then(response => response.json())
+        .then(response => res.send(response));
 });
 
 module.exports = router;
